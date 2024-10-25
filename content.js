dbe66fc700e0d206afbe3bc1605224e81c241fe8
Abhineet Jain <abhineetjain95@gmail.com>
2024-10-25 00:21:18 -0500

    Show button to toggle hide/show comments

--- content.js
+++ content.js
@@ -1,9 +1,40 @@
-const removeCommentsSection = () => {
-  const commentsSection = document.getElementById('comments');
+let areCommentsHidden = false;
+
+const hideCommentsSection = () => {
   const haveCommentsLoaded = document.querySelector('#comments #header-author')
   if (haveCommentsLoaded) {
-    commentsSection.remove();
+    const buttonElement = getButtonElement();
+    document.getElementById('comments').prepend(buttonElement);
+    buttonElement.click();
+    clearInterval(checkComments);
   }
 }
 
-setInterval(removeCommentsSection, 500);
+// Hide comments by default once they load
+const checkComments = setInterval(hideCommentsSection, 500);
+
+const getButtonElement = () => {
+  const buttonElement = document.createElement('button-view-model');
+  buttonElement.classList.add('yt-spec-button-view-model')
+  buttonElement.innerHTML = `
+    <button class="toggle-comments-button yt-spec-button-shape-next yt-spec-button-shape-next--tonal yt-spec-button-shape-next--mono yt-spec-button-shape-next--size-m" aria-disabled="false" style="">
+        <div class="yt-spec-button-shape-next__button-text-content">Hide Comments</div>
+    </button>
+  `
+  buttonElement.onclick = function() {
+    const commentsSection = document.querySelector('#comments #sections');
+    const toggleButtonText = document.querySelector('.toggle-comments-button > div');
+   
+    if (areCommentsHidden) {
+      commentsSection.style.display = 'block';
+      toggleButtonText.innerText = 'Hide Comments';
+      areCommentsHidden = false;
+    } else {
+      commentsSection.style.display = 'none';
+      toggleButtonText.innerText = 'Show Comments';
+      areCommentsHidden = true;
+    }
+  };
+
+  return buttonElement;
+}
